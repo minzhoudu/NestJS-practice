051e8770de19c8506a29da552e4a6ad43ccba249
Pavle <jovanovix.p@gmail.com>
2023-07-11 19:01:43 +0200

    implementing data exclusion with interceptors

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { SerializedUser } from '../types/User';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userService: UsersService,
+  ) {}
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:username')
+  findUserByUsername(@Param('username') username: string) {
+    const user = this.userService.getUserByUsername(username);
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return new SerializedUser(user);
+  }
+}
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,29 @@
+// import { plainToInstance } from 'class-transformer';
+import { SerializedUser, User } from './../types/User';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: Array<User> = [
+    {
+      username: 'john',
+      password: '12345',
+    },
+    {
+      username: 'pavle',
+      password: '12345',
+    },
+    {
+      username: 'pera',
+      password: '12345',
+    },
+  ];
+
+  getUsers(): Array<User> {
+    return this.users.map((user) => new SerializedUser(user));
+  }
+
+  getUserByUsername(username: string): User {
+    return this.users.find((user) => user.username === username);
+  }
+}
